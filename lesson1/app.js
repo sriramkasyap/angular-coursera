45085f2e9db8e3d3c922559fe1e9436e7b324c1c
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2016-11-24 12:57:06 +0530

    -Added Angular JS -Added a controller "MyController" into "myFirstApp"

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,27 +1,11 @@
-var student = {
-  name: "",
-  type: "student"
-};
+(function() {
+ 'use strict';
 
-document.addEventListener('DOMContentLoaded', contentLoaded);
+  x = 'hello!';
+  angular.module('myFirstApp',[])
 
-function contentLoaded(event) {
-  document.getElementById('name').addEventListener("keyup", keyUp);
-}
+  .controller('MyController',function() {
 
-function keyUp(event) {
-  calculateNumericOutput();
-}
+  });
 
-function calculateNumericOutput() {
-  student.name = document.getElementById('name').value;
-
-  var totalNameValue = 0;
-  for (var i = 0; i < student.name.length; i++) {
-    totalNameValue += student.name.charCodeAt(i);
-  }
-
-  // Insert result into page
-  var output = "Total Numeric value of person's name is " + totalNameValue;
-  document.getElementById('output').innerText = output;
-}
+})();
