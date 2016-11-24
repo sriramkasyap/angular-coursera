f3127ca1c24d0b34e5b43f2b9df13f89a7d22c4f
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2016-11-24 16:54:06 +0530

    - Added basic data binding functionality to 'MyFirstController'

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,1 +1,13 @@
+(function () {
+'use strict';
 
+angular.module('myFirstApp', [])
+
+  .controller('MyFirstController', function ($scope) {
+    $scope.name = 'Sriram Kasyap';
+    $scope.sayHello = function(name) {
+      return "Hello " + name "!!";
+    };
+  });
+
+})();
