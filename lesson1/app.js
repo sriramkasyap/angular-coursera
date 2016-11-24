14add682ce9bb5f162449f6488aa1a6bbb063dc7
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2016-11-24 16:59:23 +0530

    - Saying Hello Functioality added.

--- lesson1/app.js
+++ lesson1/app.js
@@ -6,7 +6,7 @@ angular.module('myFirstApp', [])
   .controller('MyFirstController', function ($scope) {
     $scope.name = 'Sriram Kasyap';
     $scope.sayHello = function(name) {
-      return "Hello " + name "!!";
+      return "Hello " + name + "!!";
     };
   });
 
