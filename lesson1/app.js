1ff7c0c3f157595f796cae488c80c3b38c3e18d1
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2016-11-25 13:26:04 +0530

    Copied Lecture 11 Material

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,17 +1,12 @@
-// (function () {
-// 'use strict';
-//
-//   angular.module('DIApp', [])
-//   .controller('DIController', ['$scope','$filter',DIController]);
-//
-//   function DIController ($scope, $filter) {
-//     $scope.name = "Yaakov";
-//     $scope.upper = function () {
-//       var upCase = $filter('uppercase');
-//       $scope.name = upCase($scope.name);
-//     };
-//   }
-//
-// })();
+(function () {
+'use strict';
 
-!function(){"use strict";function e(e,n){e.name="Yaakov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}angular.module("DIApp",[]).controller("DIController",["$scope","$filter",e])}();
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.name = "Yaakov";
+}
+
+})();
