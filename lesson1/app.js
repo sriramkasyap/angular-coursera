ff29b63e430be08951bdb25ea5b9a7e3bca4c48d
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2016-11-25 13:39:32 +0530

    - Built a MsgApp with MsgController - Discussed Expressions and Interpolations

--- lesson1/app.js
+++ lesson1/app.js
@@ -6,7 +6,10 @@ angular.module('MsgApp', [])
 
 MsgController.$inject = ['$scope'];
 function MsgController($scope) {
-  $scope.name = "Yaakov";
+  $scope.name = "Sriram Kasyap";
+  $scope.sayMessage = function () {
+    return "Sriram Kasyap likes to eat, sleep and code!!";
+  }
 }
 
 })();
