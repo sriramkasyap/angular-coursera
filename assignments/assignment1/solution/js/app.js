fde059395c7cb60ffd2f7caa209ef027929fabc0
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2016-11-26 16:03:17 +0530

    added Bonus Points

--- assignments/assignment1/solution/js/app.js
+++ assignments/assignment1/solution/js/app.js
@@ -10,13 +10,20 @@
     $scope.items='';
     $scope.message='';
     $scope.checkIfTooMuch = function() {
-      var stripItems = $scope.items.replace(/(^,)|(,$)/g, "")
+      var stripItems = $scope.items.replace(/(^,)|(,$)/g, "");
+      var stripItems = stripItems.replace(/(,,)|(,\s+,)/g,",");
       var allItems = stripItems.split(',');
-      if (allItems.length > 3) {
-        $scope.message = 'Too Much!';
+     if (allItems.length > 3) {
+       $scope.message = ' Too Much!';
+       $scope.messageColor = 'green';
+      }
+      else if (!stripItems.trim()) {
+        $scope.message = 'Please Enter Data First!';
+        $scope.messageColor='red';
       }
       else {
-        $scope.message = 'Enjoy!';
+        $scope.message = ' Enjoy! ';
+        $scope.messageColor = 'green';
       }
     };
   }
